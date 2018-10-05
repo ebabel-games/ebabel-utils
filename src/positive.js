e045f359a07e127e608055977142010840a0fc1e
Thalyta Fabrine <thalyta.trindade@ccc.ufcg.edu.br>
2018-10-05 19:42:47 -0300

    JSDoc documentation resolves #1

--- src/positive.js
+++ src/positive.js
@@ -1,5 +1,11 @@
 'use strict';
 
+/**
+ * `positive`
+ * Computes the positive rounded up value of the input.
+ * @param {number} input - Number to be converted into positive.
+ * @returns {number} Positive value of the input number.
+ */
 const positive = (input) =>  Math.ceil(Math.abs(input));
 
 module.exports = positive;
