d8787598a333b9d02407513133800696dac68629
Dave Pitts <dgapitss@gmail.com>
2018-10-07 18:37:59 +0200

    test coverage for random-list.test.js 95percent - one elusive branch

--- test/random-list.test.js
+++ test/random-list.test.js
@@ -6,3 +6,55 @@ test('randomList returns an object with 2 empty arrays when the input is missing
   expect(result.list).toEqual([]);
   expect(result.rest).toEqual([]);
 });
+
+test('randomList standard input', () => {
+  const input = ['orc', 'goblin', 'orc', 'orc', 'elf', 'elf'];
+  const result = randomList(input);
+  expect(result.list!=[]).toBe(true);
+  expect(result.rest!=[]).toBe(true);
+});
+
+test('input.source randomList standard input with input.max 2', () => {
+  const input = ['orc', 'goblin', 'orc', 'orc', 'elf', 'elf'];
+  const result = randomList(input,2);
+  expect(result.list!=[]).toBe(true);
+  expect(result.rest!=[]).toBe(true);
+});
+
+
+test('input.source randomList standard input with input.max 2', () => {
+  const input = {source:['orc', 'goblin', 'orc', 'orc', 'elf', 'elf'],max:2};
+  const result = randomList(input);
+  expect(result.list!=[]).toBe(true);
+  expect(result.rest!=[]).toBe(true);
+});
+
+test('input.source randomList standard input with input.max 0', () => {
+  const input = {source:['orc', 'goblin', 'orc', 'orc', 'elf', 'elf'],max:0};
+  const result = randomList(input);
+  expect(result.list!=[]).toBe(true);
+  expect(result.rest!=[]).toBe(true);
+});
+
+test('input.source randomList object input with input.max 0', () => {
+  const input = {
+    source: [
+      { beast: 'orc' },
+      { beast: 'orc' },
+      { beast: 'orc' }
+    ],
+    max: 0
+  };
+
+  const result = randomList(input);
+  expect(result.list!=[]).toBe(true);
+  expect(result.rest!=[]).toBe(true);
+});
+
+
+test('randomList empty input list', () => {
+  const input = [];
+  const result = randomList(input);
+  expect(result.list).toEqual([]);
+  expect(result.rest).toEqual([]);
+});
