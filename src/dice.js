e76f595c580450217eabfbe103eae2def444060a
Dave Pitts <dgapitss@gmail.com>
2018-10-13 11:35:58 +0200

    add jsdoc example documentation for src/dic.js

--- src/dice.js
+++ src/dice.js
@@ -5,8 +5,11 @@ const random = require('./random');
 /**
  * `dice`
  * Throw 1 die.
+ * @example <caption>Example usage of dice</caption>
+ * const result = dice();
  * @returns {number} Return a random integer from 1 to 6.
  */
+
 const dice = () => random(6);
 
 module.exports = dice;
