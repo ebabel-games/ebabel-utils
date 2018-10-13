7f15d3eda4702629a94de624a2b93580f02fab7d
Dave Pitts <dgapitss@gmail.com>
2018-10-13 11:22:25 +0200

    add jsdoc example documentation for src/random-pos-or-neg.js

--- src/random-pos-or-neg.js
+++ src/random-pos-or-neg.js
@@ -2,9 +2,12 @@
 
 const random = require('./random');
 
+
 /**
  * `randomPosOrNeg`
  * Calculates a positive or negative random number.
+ * @example <caption>Example usage of randomPosOrNeg</caption>
+ * const spellFizzleEffect = randomPosOrNeg(16);
  * @param {number} max - Maximum value that can be generated.
  * @returns {number} Positive or negative number randomly generated.
  */
