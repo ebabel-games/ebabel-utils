eaccd5d0ec6337c65f7127d9b84e7ddc1e720d69
Dave Pitts <dgapitss@gmail.com>
2018-10-17 18:00:09 +0200

    add jsdoc example documentation for src/reduced-distance.js

--- src/reduced-distance.js
+++ src/reduced-distance.js
@@ -7,6 +7,11 @@
  * @param {array} i - Array of 3 numbers for the coordinates x, y, z of the source point.
  * @param {array} j - Array of 3 numbers for the coordicates x, y, z of the target point.
  * @param {number} r - Number by which the distance between i and j needs to be reduced.
+ * @example <caption>Example usage of reducedDistance</caption>
+ * const attackingNpcPosition = [45, 23, -65];
+ * const playerPosition = [34, 0, -10];
+ * const howMuchCloser = 5;
+ * const attackingNpcNextPosition = reducedDistance(attackingNpcPosition, playerPosition, howMuchCloser);
  * @returns {array} Array of coordinates the i position should get to in order to get closer to j.
  */
 const reducedDistance = (i, j, r) => {
