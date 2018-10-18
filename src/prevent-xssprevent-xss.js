3eaff76b0d79f1b3490c7f69130752d30411e80a
Dave Pitts <dgapitss@gmail.com>
2018-10-18 13:10:38 +0100

    add example usage to src/prevent-xssprevent-xss.js

--- src/prevent-xssprevent-xss.js
+++ src/prevent-xssprevent-xss.js
@@ -4,6 +4,10 @@
  * `preventXss`
  * Processes an input string to prevent Cross Site Scripting injection attacks (XSS). Returns a safe version of that input.
  * @param {string} input - The user input string that needs to be sanitized, to remove the possibility of injecting script of other html tags.
+ * @example <caption>Example usage of preventXss</caption>
+ * const safe = '&lt;script&gt;&lt;/script&gt;';
+ * const result = preventXss('<script></script>');
+ * expect(result).toBe(safe);
  * @returns {string} Cleaned string that shouldn't run any javascript code if displayed on a webpage.
  */
 const preventXss = (input) => input.replace(/</g, '&lt;').replace(/>/g, '&gt;');
