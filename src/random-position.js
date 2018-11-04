ab40e7698789b7fc15be75c673cd38f1e6531236
Thomas Amar <hello@ebabel.eu>
2018-11-04 07:20:08 +0100

    Add new parameter to randomPosition.

--- src/random-position.js
+++ src/random-position.js
@@ -5,14 +5,15 @@ const randomPosOrNeg = require('./random-pos-or-neg');
 /**
  * `randomPosition`
  * Calculate new coordinates in a 3D space randomly to generate a new position.
- * @param {array} size - Array of 3 numbers for the width, height and depth, respectively.
+ * @param {Array?} size - Array of 3 numbers for the width, height and depth, respectively. Default is [6000, 6000, 6000].
+ * @param {Array?} exclusion - Array of 3 numbers for the area around origin that should be excluded from possible random positions. Default is [0, 0, 0].
  * @example <caption>Example usage of randomPosition</caption>
  * const spawnAreaSize = [3000, 2000, 3000];
  * const spawnPosition = randomPosition(spawnAreaSize);
- * @returns {array} Array of generated position coordinates.
+ * @returns {Array} Array of generated position coordinates.
  */
 
-const randomPosition = (size = [6000, 6000, 6000]) => {
+const randomPosition = (size = [6000, 6000, 6000], exclusion = [0, 0, 0]) => {
   const width = size[0];
   const height = size[1];
   const depth = size[2];
